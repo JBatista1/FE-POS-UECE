9556e211925a635dfd30a313401bd674e43a6b42
João Batista <j.batista.damasceno@icloud.com>
2024-06-24 16:39:33 -0300

    Ajuste endpoint

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -15,5 +15,5 @@ export const Environment = {
   /**
      * Url base de consultado dos dados dessa aplicação
      */
-  URL_BASE: 'http://ec2-3-92-221-141.compute-1.amazonaws.com:8087',
+  URL_BASE: 'http://ec2-44-212-38-175.compute-1.amazonaws.com:8087',
 };
